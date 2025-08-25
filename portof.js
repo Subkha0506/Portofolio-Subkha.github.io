96ab075173cd0dc93f9c88fa06a07a9a1cf00d2e
Subkha <subhamuhammad95@gmail.com>
2025-08-25 14:44:47 +0700

    update

--- portof.js
+++ portof.js
@@ -1,39 +1,47 @@
 function toggleDarkMode() {
-      document.body.classList.toggle("dark");
-      const btn = document.querySelector(".toggle-btn");
-      if (document.body.classList.contains("dark")) {
-        btn.textContent = "☀️ Light Mode";
-      } else {
-        btn.textContent = "🌙 Dark Mode";
-      }
+  document.body.classList.toggle("dark");
+  const btn = document.querySelector(".toggle-btn");
+  if (document.body.classList.contains("dark")) {
+    btn.textContent = "☀️ Light Mode";
+  } else {
+    btn.textContent = "🌙 Dark Mode";
+  }
+}
+
+const text1 = "Perancang desain antarmuka pengguna (UI) dan sistem desain.";
+const text2 = "Pengembang front-end yang menulis kode yang bersih, elegan, dan efisien.";
+let i = 0, j = 0;
+
+function typeFirst() {
+    if (i < text1.length) {
+      document.getElementById("typing1").textContent += text1.charAt(i);
+      i++;
+      setTimeout(typeFirst, 50);
+    
+
+    if (j < text2.length) {
+      document.getElementById("typing2").textContent += text2.charAt(j);
+      j++;
+      setTimeout(typeSecond, 50);
     }
-
-    const text1 = "Perancang desain antarmuka pengguna (UI) dan sistem desain.";
-    const text2 = "Pengembang front-end yang menulis kode yang bersih, elegan, dan efisien..";
-    let i = 0, j = 0;
-
-    function typeFirst() {
-      if (i < text1.length) {
-        document.getElementById("typing1").textContent += text1.charAt(i);
-        i++;
-        setTimeout(typeFirst, 50);
-      } 
-
-      if (j < text2.length) {
-        document.getElementById("typing2").textContent += text2.charAt(j);
-        j++;
-        setTimeout(typeSecond, 50);
-      } 
-    }
+  }
+}
 
 
-    window.onload = typeFirst;
+window.onload = typeFirst;
 
 function downloadCV() {
+  const btn = document.querySelector(".start-button");
+  btn.textContent = "⏳ Mengunduh...";
+
   const link = document.createElement("a");
   link.href = "CV_Muhammad_Subkha.pdf"; 
   link.download = "CV_Muhammad_Subkha.pdf"; 
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+
+  setTimeout(() => {
+    btn.textContent = "✅ CV Terunduh";
+  }, 2000);
 }
